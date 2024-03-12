85f2df9d2e21d6a7c0a5f36e38166511203e1b5d
suyashpanwar <suyashp124@gmail.com>
2024-03-12 22:26:11 +0530

    admin, user design

--- src/AdminPage.js
+++ src/AdminPage.js
@@ -6,8 +6,8 @@ export default function AdminPage(){
                     <p>Admin Profile</p>
                 </div>
                 <div className="current-admin">
-                    <h3>username : suyashpanwar</h3>
-                    <h3>password : suyashpanwar</h3>
+                    <h3>email : suyashp124@gmail.com</h3>
+                    <h3>password : Suyash</h3>
                 </div>
             </div>
             <div>
@@ -31,6 +31,19 @@ export default function AdminPage(){
             <div>
                 <div className="nav-bar">
                     <p>Remove User</p>
+                </div>
+                <div>
+                    <div>
+                        <label htmlFor="new-email" className="labels-new">Email</label>
+                        <input type="email" id="new-email" className="new-email"/>
+                    </div>
+                    <div>
+                        <label htmlFor="new-password" className="labels-new">Password</label>
+                        <input type="password" id="new-password" className="new-password" />
+                    </div>
+                    <div className="button-new">
+                        <button className="button">Remove User</button>
+                    </div>  
                 </div>
             </div>
         </div>
--- src/UserPage.js
+++ src/UserPage.js
@@ -1,9 +1,39 @@
 export default function UserPage(){
     return(
         <div>
-            <h3>
-                Hello Users
-            </h3>
+            <div>
+                <div className="nav-bar">
+                    <p>User Profile</p>
+                </div>
+                <div className="current-admin">
+                    <h3>email : suyashp124@gmail.com</h3>
+                    <h3>password : Suyash</h3>
+                </div>
+            </div>
+            <div>
+                <div className="nav-bar">
+                    <p>Send attendance request</p>
+                </div>
+                <div>
+                    <label htmlFor="class-name" className="labels-user">Class</label>
+                    <input type="text" id="class-name" placeholder="enter class ID" className="new-class"/>
+                </div>
+                <div className="button-new">
+                    <button className="button">Send Request</button>
+                </div>
+            </div>
+            <div>
+                <div className="nav-bar">
+                    <p>Add new class</p>
+                </div>
+                <form action="/action_page.php" className="class-new">
+                    <label htmlFor="class-name" >Class</label>
+                    <input type="text" id="class-name" placeholder="new class ID" className="new-class"/>
+                    <p>attach excel file for new class</p>
+                    <input type="file" id="myFile" name="filename" />
+                    <input type="submit" className="button" />
+                </form>
+            </div>
         </div>
     )
 }
